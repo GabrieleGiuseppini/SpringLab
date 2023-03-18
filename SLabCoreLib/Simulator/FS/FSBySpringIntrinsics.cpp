e5791a5da3feca3d54fcbe9fd9dc8a34c1b83b6c
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2023-03-18 12:14:46 +0100

    By springs with intrinsics - take 1

--- SLabCoreLib/Simulator/FS/FSBySpringIntrinsics.cpp
+++ SLabCoreLib/Simulator/FS/FSBySpringIntrinsics.cpp
@@ -5,6 +5,8 @@
 ***************************************************************************************/
 #include "FSBySpringIntrinsics.h"
 
+#include "SysSpecifics.h"
+
 #include <cassert>
 
 FSBySpringIntrinsics::FSBySpringIntrinsics(
@@ -124,12 +126,112 @@ void FSBySpringIntrinsics::ApplySpringsForces(Object const & object)
     float const * restrict const stiffnessCoefficientBuffer = mSpringStiffnessCoefficientBuffer.data();
     float const * restrict const dampingCoefficientBuffer = mSpringDampingCoefficientBuffer.data();
 
-    // TODOHERE
+    __m128 const Zero = _mm_setzero_ps();
+
     ElementCount const springCount = object.GetSprings().GetElementCount();
-    for (ElementIndex springIndex = 0; springIndex < springCount; ++springIndex)
+    ElementCount const springVectorizedCount = springCount - (springCount % 4);
+    ElementIndex s = 0;
+
+    // Word-by-word
+    for (; s < springVectorizedCount; s += 4)
     {
-        auto const pointAIndex = endpointsBuffer[springIndex].PointAIndex;
-        auto const pointBIndex = endpointsBuffer[springIndex].PointBIndex;
+        //
+        // Calculate displacement
+        // 
+        
+        // Spring 0 displacement
+        __m128 const s0pa_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 0].PointAIndex)));
+        __m128 const s0pb_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 0].PointBIndex)));
+        // pos[s0.B].x - pos[s0.A].x, pos[s0.B].y - pos[s0.A].y, *, *
+        __m128 const s0_displacement = _mm_sub_ps(s0pb_pos, s0pa_pos);
+
+        // Spring 1 displacement
+        __m128 const s1pa_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 1].PointAIndex)));
+        __m128 const s1pb_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 1].PointBIndex)));
+        // pos[s1.B].x - pos[s1.A].x, pos[s1.B].y - pos[s1.A].y, *, *
+        __m128 const s1_displacement = _mm_sub_ps(s1pb_pos, s1pa_pos);
+
+        // s0_displacement.x, s0_displacement.y, s1_displacement.x, s1_displacement.y
+        __m128 const s0s1_displacement = _mm_movelh_ps(s0_displacement, s1_displacement);
+
+        // Spring 2 displacement
+        __m128 const s2pa_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 2].PointAIndex)));
+        __m128 const s2pb_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 2].PointBIndex)));
+        // pos[s2.B].x - pos[s2.A].x, pos[s2.B].y - pos[s2.A].y, *, *
+        __m128 const s2_displacement = _mm_sub_ps(s2pb_pos, s2pa_pos);
+
+        // Spring 3 displacement
+        __m128 const s3pa_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 3].PointAIndex)));
+        __m128 const s3pb_pos = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<double const * restrict>(pointPositionBuffer + endpointsBuffer[s + 3].PointBIndex)));
+        // pos[s3.B].x - pos[s3.A].x, pos[s3.B].y - pos[s3.A].y, *, *
+        __m128 const s3_displacement = _mm_sub_ps(s3pb_pos, s3pa_pos);
+
+        // s2_displacement.x, s2_displacement.y, s3_displacement.x, s3_displacement.y
+        __m128 const s2s3_displacement = _mm_movelh_ps(s2_displacement, s3_displacement);
+
+        // Shuffle displacements:
+        // s0_displacement.x, s1_displacement.x, s2_displacement.x, s3_displacement.x
+        __m128 s0s1s2s3_displacement_x = _mm_shuffle_ps(s0s1_displacement, s2s3_displacement, 0x88);
+        // s0_displacement.y, s1_displacement.y, s2_displacement.y, s3_displacement.y
+        __m128 s0s1s2s3_displacement_y = _mm_shuffle_ps(s0s1_displacement, s2s3_displacement, 0xDD);
+
+        //
+        // Calculate spring lengths
+        //
+
+        // s0_displacement.x^2, s1_displacement.x^2, s2_displacement.x^2, s3_displacement.x^2
+        __m128 const s0s1s2s3_displacement_x2 = _mm_mul_ps(s0s1s2s3_displacement_x, s0s1s2s3_displacement_x);
+        // s0_displacement.y^2, s1_displacement.y^2, s2_displacement.y^2, s3_displacement.y^2
+        __m128 const s0s1s2s3_displacement_y2 = _mm_mul_ps(s0s1s2s3_displacement_y, s0s1s2s3_displacement_y);
+
+        // s0_displacement.x^2 + s0_displacement.y^2, s1_displacement.x^2 + s1_displacement.y^2, s2_displacement..., s3_displacement...
+        __m128 const s0s1s2s3_displacement_x2_p_y2 = _mm_add_ps(s0s1s2s3_displacement_x2, s0s1s2s3_displacement_y2);
+
+        __m128 const s0s1s2s3_springLength = _mm_sqrt_ps(s0s1s2s3_displacement_x2_p_y2); // sqrt
+
+        //
+        // Calculate spring directions
+        //
+
+        __m128 const validMask = _mm_cmpneq_ps(s0s1s2s3_springLength, Zero);
+
+        __m128 s0s1s2s3_dir_x = _mm_div_ps(s0s1s2s3_displacement_x, s0s1s2s3_springLength);
+        __m128 s0s1s2s3_dir_y = _mm_div_ps(s0s1s2s3_displacement_y, s0s1s2s3_springLength);
+        // L==0 => 1/L == 0, to maintain normalized == (0, 0), as in vec2f
+        s0s1s2s3_dir_x = _mm_and_ps(s0s1s2s3_dir_x, validMask);
+        s0s1s2s3_dir_y = _mm_and_ps(s0s1s2s3_dir_y, validMask);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////
+
+        //
+        // 1. Hooke's law
+        //
+
+        // Calculate springs' forces' moduli - for endpoints A:
+        //    (displacementLength[s] - restLength[s]) * stiffness[s]
+
+        __m128 const s0s1s2s3_restLength = _mm_load_ps(restLengthBuffer + s);
+        __m128 const s0s1s2s3_stiffness = _mm_load_ps(stiffnessCoefficientBuffer + s);
+
+        __m128 const s0s1s2s3_hooke_forceModuli = _mm_mul_ps(
+            _mm_sub_ps(s0s1s2s3_springLength, s0s1s2s3_restLength),
+            s0s1s2s3_stiffness);
+
+        //
+        // 2. Damper forces
+        //
+        // Damp the velocities of each endpoint pair, as if the points were also connected by a damper
+        // along the same direction as the spring
+        //
+
+        // TODOHERE
+    }
+
+    // One-by-one
+    for (; s < springCount; ++s)
+    {
+        auto const pointAIndex = endpointsBuffer[s].PointAIndex;
+        auto const pointBIndex = endpointsBuffer[s].PointBIndex;
 
         vec2f const displacement = pointPositionBuffer[pointBIndex] - pointPositionBuffer[pointAIndex];
         float const displacementLength = displacement.length();
@@ -141,13 +243,13 @@ void FSBySpringIntrinsics::ApplySpringsForces(Object const & object)
 
         // Calculate spring force on point A
         float const fSpring =
-            (displacementLength - restLengthBuffer[springIndex])
-            * stiffnessCoefficientBuffer[springIndex];
+            (displacementLength - restLengthBuffer[s])
+            * stiffnessCoefficientBuffer[s];
 
         //
         // 2. Damper forces
         //
-        // Damp the velocities of the two points, as if the points were also connected by a damper
+        // Damp the velocities of each endpoint pair, as if the points were also connected by a damper
         // along the same direction as the spring
         //
 
@@ -155,7 +257,7 @@ void FSBySpringIntrinsics::ApplySpringsForces(Object const & object)
         vec2f const relVelocity = pointVelocityBuffer[pointBIndex] - pointVelocityBuffer[pointAIndex];
         float const fDamp =
             relVelocity.dot(springDir)
-            * dampingCoefficientBuffer[springIndex];
+            * dampingCoefficientBuffer[s];
 
         //
         // Apply forces
